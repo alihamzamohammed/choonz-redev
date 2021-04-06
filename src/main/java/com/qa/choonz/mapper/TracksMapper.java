4916b54e4e6e462f5b4db01e038da97cbc6636d9
Ifan Clarke <Ifan>
2021-04-06 15:48:00 +0100

    tracks mapper completed

---
+++ src/main/java/com/qa/choonz/mapper/TracksMapper.java
@@ -0,0 +1,25 @@
+package com.qa.choonz.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.qa.choonz.persistence.domain.Track;
+import com.qa.choonz.rest.dto.TrackDTO;
+
+
+public class TracksMapper {
+private ModelMapper modelMapper;
+	
+	@Autowired
+	public TracksMapper (ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+	}
+	
+	public TrackDTO mapToDTO(Track track) {
+		return this.modelMapper.map(track, TrackDTO.class);
+	}
+	
+	public Track mapToGenre(TrackDTO trackDTO) {
+        return this.modelMapper.map(trackDTO, Track.class);
+	}
+}
