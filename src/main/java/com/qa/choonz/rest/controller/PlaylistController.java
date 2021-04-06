b101731268d51c6a259501b85cc60ccabb532ff8
ChrisQA-GB <cyiangou@qa.com>
2021-04-06 14:58:03 +0100

    CR-43 #comment Playlist Controller updated

--- src/main/java/com/qa/choonz/rest/controller/PlaylistController.java
+++ src/main/java/com/qa/choonz/rest/controller/PlaylistController.java
@@ -2,6 +2,7 @@ package com.qa.choonz.rest.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
