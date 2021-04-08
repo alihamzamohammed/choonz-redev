f4fa8ef02367ae5c6bcf1efe72f84f19e90d31f5
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-08 17:57:15 +0100

    Removed unused import

--- src/main/java/com/qa/choonz/persistence/domain/User.java
+++ src/main/java/com/qa/choonz/persistence/domain/User.java
@@ -1,12 +1,9 @@
 package com.qa.choonz.persistence.domain;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
