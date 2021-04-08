cb0d466c50f9aeedb3294b703a8aaad55319d98d
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-08 20:32:45 +0100

    Added usermanagement.js to all html pages

--- src/main/java/com/qa/choonz/service/SessionService.java
+++ src/main/java/com/qa/choonz/service/SessionService.java
@@ -1,6 +1,5 @@
 package com.qa.choonz.service;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -10,9 +9,6 @@ public class SessionService {
 
     public String getUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        // if (authentication instanceof AnonymousAuthenticationToken) {
-        // return "Not logged in";
-        // }
         return authentication.getName();
     }
 }
