039b32030784360c6542bbf739988acb64949325
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-09 16:01:24 +0100

    CR-98 Search controller working

--- src/main/java/com/qa/choonz/service/SearchService.java
+++ src/main/java/com/qa/choonz/service/SearchService.java
@@ -2,16 +2,21 @@ package com.qa.choonz.service;
 
 import java.util.List;
 
-import com.qa.choonz.persistence.domain.Album;
-import com.qa.choonz.persistence.domain.Artist;
-import com.qa.choonz.persistence.domain.Genre;
-import com.qa.choonz.persistence.domain.Playlist;
-import com.qa.choonz.persistence.domain.Track;
+import com.qa.choonz.mapper.AlbumMapper;
+import com.qa.choonz.mapper.ArtistMapper;
+import com.qa.choonz.mapper.GenreMapper;
+import com.qa.choonz.mapper.PlaylistMapper;
+import com.qa.choonz.mapper.TracksMapper;
 import com.qa.choonz.persistence.repository.AlbumRepository;
 import com.qa.choonz.persistence.repository.ArtistRepository;
 import com.qa.choonz.persistence.repository.GenreRepository;
 import com.qa.choonz.persistence.repository.PlaylistRepository;
 import com.qa.choonz.persistence.repository.TrackRepository;
+import com.qa.choonz.rest.dto.AlbumDTO;
+import com.qa.choonz.rest.dto.ArtistDTO;
+import com.qa.choonz.rest.dto.GenreDTO;
+import com.qa.choonz.rest.dto.PlaylistDTO;
+import com.qa.choonz.rest.dto.TrackDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,23 +39,38 @@ public class SearchService {
     @Autowired
     private TrackRepository trackRepo;
 
-    public List<Album> searchAlbums(String name) {
-        return albumRepo.findByName(name);
+    @Autowired
+    private ArtistMapper artistMapper;
+
+    @Autowired
+    private AlbumMapper albumMapper;
+
+    @Autowired
+    private GenreMapper genreMapper;
+
+    @Autowired
+    private PlaylistMapper playlistMapper;
+
+    @Autowired
+    private TracksMapper tracksMapper;
+
+    public List<AlbumDTO> searchAlbums(String name) {
+        return albumMapper.mapToDTO(albumRepo.findByName(name));
     }
 
-    public List<Artist> searchArtists(String name) {
-        return artistRepo.findByName(name);
+    public List<ArtistDTO> searchArtists(String name) {
+        return artistMapper.listMapToDTO(artistRepo.findByName(name));
     }
 
-    public List<Genre> searchGenres(String name) {
-        return genreRepo.findByName(name);
+    public List<GenreDTO> searchGenres(String name) {
+        return genreMapper.listMapToDTO(genreRepo.findByName(name));
     }
 
-    public List<Playlist> searchPlaylists(String name) {
-        return playlistRepo.findByName(name);
+    public List<PlaylistDTO> searchPlaylists(String name) {
+        return playlistMapper.mapToDTO(playlistRepo.findByName(name));
     }
 
-    public List<Track> searchTracks(String name) {
-        return trackRepo.findByName(name);
+    public List<TrackDTO> searchTracks(String name) {
+        return tracksMapper.listMapToDTO(trackRepo.findByName(name));
     }
 }
