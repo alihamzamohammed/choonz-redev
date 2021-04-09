49e429852b0d5d0ed25be5c895545dedfe3d4e15
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-09 15:05:24 +0100

    CR-102 Find by name methods in repos

--- src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
+++ src/main/java/com/qa/choonz/persistence/repository/AlbumRepository.java
@@ -3,9 +3,13 @@ package com.qa.choonz.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.qa.choonz.persistence.domain.Album;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
+
+    List<Album> findByName(String name);
 
 }
--- src/main/java/com/qa/choonz/persistence/repository/GenreRepository.java
+++ src/main/java/com/qa/choonz/persistence/repository/GenreRepository.java
@@ -3,9 +3,12 @@ package com.qa.choonz.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.qa.choonz.persistence.domain.Genre;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
 
+    List<Genre> findByName(String name);
 }
