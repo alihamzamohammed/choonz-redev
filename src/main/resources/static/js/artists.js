ed3c6478b064e043d4de143f99138a98954a5f65
peter5600 <peter.colquhoun3@gmail.com>
2021-04-13 17:53:33 +0100

    Added JS Changes to Genre and artist(s) page

--- src/main/resources/static/js/artists.js
+++ src/main/resources/static/js/artists.js
@@ -1,74 +1,69 @@
 'use strict'
 
-const createArtist = document.querySelector("#CreateArtistButton");
-createArtist.addEventListener("click", (event) => {
-  event.preventDefault();
-  CreateArtist();
-});
-
 //Creating Create Functionality for Artists
 const CreateArtist = () => {
-    let artist = document.querySelector("#ArtistList").value;
-    console.log(artist);
-    console.log(artistName);
-    console.log(genre);
-  
-    const obj = {
-      name: artistName,
-      genre: genre,
-    };
-  
-    fetch("http://localhost:8082/artist", {
+  console.log(" I have tried to Submitted")
+  let ArtistName = document.querySelector("#ArtistName").value
+  console.log(ArtistName + " hello")
+  fetch("http://localhost:8082/artists/create", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(obj),
+      body: JSON.stringify({
+        "name": ArtistName
+      }),
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.err(err));
-  };
+    .then((res) => {
+      if (res.status === 201) {
+        location.reload()
+      } else {
+        throw "The artist wasn't created error"
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
-  // read all artists
+// read all artists
 (function () {
-    fetch(`http://localhost:8082/artists/read`, {
+  fetch(`http://localhost:8082/artists/read`, {
       method: "GET",
     })
-      .then((response) => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw "Response code was not 200 it was " + response.status;
-        }
-      })
-      .then((data) => {
-        data.forEach((playlist) => {
-          var ArtistElement = document.createElement("div");
-          var name = artist.name;
-          var genre = artist.genre;
-  
-          ArtistElement.className = "ListItem col-2 ms-5 mb-5 text-center mt-5";
-          ArtistElement.style = "border-radius: 12px;";
-  
-          ArtistElement.innerHTML = `
-             
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw "Response code was not 200 it was " + response.status;
+      }
+    })
+    .then((data) => {
+      data.forEach((Artist) => {
+        var ArtistElement = document.createElement("div");
+        var name = Artist.name;
+        console.log(JSON.stringify(data))
+        ArtistElement.className = "ListItem col-2 ms-5 mb-5 text-center mt-5";
+        ArtistElement.style = "border-radius: 12px;";
+        let ID = data[0].id;
+        let URL = window.location
+        let BaseURL = URL.protocol + "//" + URL.host;
+        let FinalURL = BaseURL + `/artist?ArtistID=${ID}`
+        ArtistElement.innerHTML = `
+                  <a href="${FinalURL}">
                   <div class="text-center">
                       <h4>${name}</h4>
-                      <h4>${genre} Name</h4>
+                      <h4>Album Count</h4>
+                      <h4>Most Popular Genre</h4>
                   </div>
-              
+              </a>
               `;
-  
-          document.querySelector("#ArtistList").append(ArtistElement);
-        });
-      })
-      .catch((err) => {
-        alert(
-          "There was a problem getting the albums from the system. Please try again later." +
-            err
-        );
-      });
-  })();
 
-  
+        document.querySelector("#ArtistList").append(ArtistElement);
+      });
+    })
+    .catch((err) => {
+      alert(
+        "There was a problem getting the albums from the system. Please try again later." +
+        err
+      );
+    });
+})();
