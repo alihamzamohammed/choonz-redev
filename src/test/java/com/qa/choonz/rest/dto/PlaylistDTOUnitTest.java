f6cfb440f2c8882859bc5cba2e28a0cc5148ce55
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-14 15:56:14 +0100

    DTO fix

--- src/test/java/com/qa/choonz/rest/dto/PlaylistDTOUnitTest.java
+++ src/test/java/com/qa/choonz/rest/dto/PlaylistDTOUnitTest.java
@@ -6,39 +6,33 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import com.qa.choonz.rest.dto.AlbumDTO;
-import com.qa.choonz.rest.dto.ArtistDTO;
-import com.qa.choonz.rest.dto.ArtistRelationshipDTO;
-import com.qa.choonz.rest.dto.PlaylistDTO;
-
 import nl.jqno.equalsverifier.EqualsVerifier;
 
-public class PlaylistDTOUnitTest {
-	
+class PlaylistDTOUnitTest {
+
 	@Test
-	public void equalsVerify() {
-		
-		 EqualsVerifier.simple().forClass(PlaylistDTO.class)
-		 .withPrefabValues(AlbumDTO.class, new AlbumDTO("Name", List.of(), List.of(), "Cover", new ArtistRelationshipDTO()), new AlbumDTO())
-	    .withPrefabValues(PlaylistDTO.class, new PlaylistDTO("Name", "Description", "Artwork", List.of()),
-				new PlaylistDTO())
-	    .withPrefabValues(ArtistDTO.class, new ArtistDTO("Name", List.of(), List.of()), new ArtistDTO()).verify();
-		
+	void equalsVerify() {
+
+		EqualsVerifier.simple().forClass(PlaylistDTO.class).withPrefabValues(AlbumDTO.class,
+				new AlbumDTO("Name", List.of(), List.of(), "Cover", new ArtistRelationshipDTO()), new AlbumDTO())
+				.withPrefabValues(PlaylistDTO.class, new PlaylistDTO("Name", "Description", "Artwork", List.of()),
+						new PlaylistDTO())
+				.withPrefabValues(ArtistDTO.class, new ArtistDTO("Name", List.of(), List.of()), new ArtistDTO())
+				.verify();
+
 	}
-	
+
 	@Test
-	public void toStringTest() {
+	void toStringTest() {
 		StringBuilder builder = new StringBuilder();
 		PlaylistDTO playlistDTO = new PlaylistDTO(1, "Name", "Description", "Artwork", List.of());
-		
-	     builder.append("PlaylistDTO [id=").append(playlistDTO.getId()).append(", name=").append(playlistDTO.getName()).append(", description=")
-         .append(playlistDTO.getDescription()).append(", artwork=").append(playlistDTO.getArtwork()).append(", tracks=").append(playlistDTO.getTracks())
-         .append("]");
-	     
-	     assertThat(playlistDTO.toString()).hasToString(builder.toString());
-	    
+
+		builder.append("PlaylistDTO [id=").append(playlistDTO.getId()).append(", name=").append(playlistDTO.getName())
+				.append(", description=").append(playlistDTO.getDescription()).append(", artwork=")
+				.append(playlistDTO.getArtwork()).append(", tracks=").append(playlistDTO.getTracks()).append("]");
+
+		assertThat(playlistDTO.toString()).hasToString(builder.toString());
+
 	}
 
 }
-
-
