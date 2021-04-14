3eed1f3ebb00988e2fdcc2a194f7d09a88df5ab9
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-04-14 11:07:53 +0100

    CR-115 Create method

--- src/test/java/com/qa/choonz/service/AlbumServiceIntegrationTest.java
+++ src/test/java/com/qa/choonz/service/AlbumServiceIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.qa.choonz.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +17,7 @@ import com.qa.choonz.rest.dto.GenreRelationshipDTO;
 import com.qa.choonz.rest.dto.TrackRelationshipDTO;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -36,21 +39,34 @@ public class AlbumServiceIntegrationTest {
     private Album validAlbum;
     private AlbumDTO validAlbumDTO;
 
+    private List<Track> listOfTracks;
+    private Artist validArtist;
+    private List<Genre> listOfGenres;
+
     @BeforeEach
-    private void init() {
-        List<Track> listOfTracks = Arrays.asList(new Track(1, "Till I Collapse", 298, "A lot of lyrics here"));
-        Artist validArtist = new Artist(1, "Eminem");
-        List<Genre> listOfGenres = Arrays.asList(new Genre(1, "Hip Hop", "A genre based around rap music."));
+    void init() {
+        listOfTracks = Arrays.asList(new Track(1, "Till I Collapse", 298, "A lot of lyrics here"));
+        validArtist = new Artist(1, "Eminem");
+        listOfGenres = Arrays.asList(new Genre(1, "Hip Hop", "A genre based around rap music."));
         validAlbum = new Album(1, "The Eminem Show", listOfTracks, validArtist, listOfGenres, "eminemshow.jpg");
 
         List<TrackRelationshipDTO> listOfTrackDTOs = Arrays
                 .asList(new TrackRelationshipDTO(1, "Till I Collapse", 298, "A lot of lyrics here"));
+
         ArtistRelationshipDTO validArtistDTO = new ArtistRelationshipDTO(1, "Eminem");
+
         List<GenreRelationshipDTO> listOfGenreDTOs = Arrays
                 .asList(new GenreRelationshipDTO(1, "Hip Hop", "A genre based around rap music."));
+
         validAlbumDTO = new AlbumDTO(1, "The Eminem Show", listOfTrackDTOs, listOfGenreDTOs, "eminemshow.jpg",
                 validArtistDTO);
 
+    }
+
+    @Test
+    void createAlbumTest() {
+        Album newAlbum = new Album("Hello World", listOfTracks, validArtist, listOfGenres, "test.png");
+        assertThat(service.create(newAlbum)).isEqualTo(mapper.mapToDTO(newAlbum));
     }
 
 }
