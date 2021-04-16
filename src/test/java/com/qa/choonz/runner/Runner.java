3e87df416f3b8a26e3242cedda4560cc674df740
peter5600 <peter.colquhoun3@gmail.com>
2021-04-16 13:52:26 +0100

    Cucumber things

--- src/test/java/com/qa/choonz/runner/Runner.java
+++ src/test/java/com/qa/choonz/runner/Runner.java
@@ -5,10 +5,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-plugin = {"pretty", "html:target/htmlReports/cucumber-report.html", "json:target/cucumber.json"},
-features = {"src/test/resources/cucumber/HomePage.feature"},
+plugin = {"pretty"},//"html:target/htmlReports/cucumber-report.html"
+features = {"src/test/resources/cucumber"},
 monochrome = true,
-glue="tests")
+glue="com.qa.choonz.tests")
 public class Runner {
 
 }
